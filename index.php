5d18d80ee2f136614ceddfacd98122acae4034d3
kxusxxjxjzj56 <lucha6900@gmail.com>
2025-10-30 10:22:38 -0500

    Added and Fixed the missing index.php

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
-include './includes/head.php';
-include './includes/topNav.php';
-include './includes/hero.php';
-include './includes/blog.php';
-include './includes/meta.php';
-//include './includes/partners.php';
-include './includes/bottomNav.php';
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/dashboard/');
+	exit;
+?>
+Something is wrong with the XAMPP installation :-(
